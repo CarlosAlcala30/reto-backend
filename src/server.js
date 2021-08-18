c7e7a86cd382ed0e32ba993c675011236b0f0521
CarlosAlcala <alcala1500@gmail.com>
2021-08-18 15:46:34 -0500

    insyal cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 //definicion del servidor
 const express = require('express');
 const server = express();
+const log = require('./middleware/log');
 
 //middleware
 server.use(express.json());
+//server.use(log(request,response,next));
+
+//routers
 
 
 module.exports = server;
